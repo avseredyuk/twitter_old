8b3cfd8bdc11ddf57790230a765c0f590c53f9ad
avsereduik <a.v.seredyuk@gmail.com>
2017-04-03 17:56:45 +0300

    second context added

--- src/main/java/com/avseredyuk/SpringTwitterRunner.java
+++ src/main/java/com/avseredyuk/SpringTwitterRunner.java
@@ -1,19 +1,41 @@
 package com.avseredyuk;
 
+import com.avseredyuk.domain.Tweet;
+import com.avseredyuk.domain.TweetService;
 import com.avseredyuk.repository.TweetRepository;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.stream.Stream;
 
 /**
  * Created by Anton_Serediuk on 4/3/2017.
  */
 public class SpringTwitterRunner {
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx =
-                new ClassPathXmlApplicationContext("spring.xml");
+        ConfigurableApplicationContext repoContext =
+                new ClassPathXmlApplicationContext(new String[]{"spring.xml"});
 
-        TweetRepository tweetRepository = (TweetRepository) ctx.getBean("tweetRepository");
+        Stream.of(repoContext.getBeanDefinitionNames())
+                .map(repoContext.getBeanFactory()::getBeanDefinition)
+                .forEach(System.out::println);
+
+        TweetRepository tweetRepository = (TweetRepository) repoContext.getBean("tweetRepository");
         tweetRepository.findAll().forEach(System.out::println);
+
+
+
+        ConfigurableApplicationContext serviceContext =
+                new ClassPathXmlApplicationContext(new String[]{"serviceConfig.xml"}, repoContext);
+
+        Stream.of(serviceContext.getBeanDefinitionNames())
+                .map(serviceContext.getBeanFactory()::getBeanDefinition)
+                .forEach(System.out::println);
+
+        TweetService tweetService = (TweetService)serviceContext.getBean("tweetService");
+        tweetService.doSomething();
+
+
 
     }
 }
